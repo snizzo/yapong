a772159473f2f9b9aefe59c96526b8e2a7e36323
Lara Vignotto <lara.vignotto@gmail.com>
2018-08-23 23:52:11 +0200

    Added bars and ball

--- ball.py
+++ ball.py
@@ -1,6 +1,10 @@
 ## here goes the code that renders and handles the ball
 
 class Ball():
-    def __init__(self):
-        pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def draw_bar:
+        pygame.draw.circle(windowSurface, WHITE, [x, y], 15)
     
--- bars.py
+++ bars.py
@@ -1,6 +1,10 @@
 ## here goes the code that renders and handles the bars
 
 class Bar():
-    def __init__(self):
-        pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def draw_bar:
+        pygame.draw.rect(windowSurface, WHITE, [x, y, 20, 100])
     
