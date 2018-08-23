ab15782700d071598d210d2e09b366f42378d93b
Lara Vignotto <lara.vignotto@gmail.com>
2018-08-24 00:20:03 +0200

    Added to bars and ball

--- ball.py
+++ ball.py
@@ -4,18 +4,6 @@ class Ball():
     def __init__(self, x, y):
         self.circle = pygame.circle.Circle((x, y, 15))
         
-    def handle_keys(self):
-        key = pygame.key.get_pressed()
-        dist = 1
-        if key[pygame.K_LEFT]:
-           self.circle.move_ip(-1, 0)
-        if key[pygame.K_RIGHT]:
-           self.circle.move_ip(1, 0)
-        if key[pygame.K_UP]:
-           self.circle.move_ip(0, -1)
-        if key[pygame.K_DOWN]:
-           self.circle.move_ip(0, 1)
-        
     def draw(self, surface):
         pygame.draw.circle(windowSurface, WHITE, self.circle)
     
